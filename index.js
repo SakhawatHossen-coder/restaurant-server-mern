7c5a36e1e658ca11ecf5429b740599cb1e543d9e
Sakhawat Hossen Sakib <sakibsakhawat21@gmail.com>
2024-05-12 15:55:04 +0600

    add food with id

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
 const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
+const port = process.env.PORT || 5000;
 
 const corsOptions = {
   origin: ["http://localhost:5173", "http://localhost:5174", ""],
@@ -28,17 +30,28 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//start 
+    //     await client.connect();
+    //start
+    const foodCollection = client.db("foodDB").collection("foods");
 
+    app.post("/addfood", async (req, res) => {
+      const newFood = req.body;
+      const result = await foodCollection.insertOne(newFood);
+      res.send(result);
+    });
 
-
-
-
-
-
-
-// comment 
+    app.get("/addfood", async (req, res) => {
+      const cursor = foodCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/addfood/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await foodCollection.findOne(query);
+      res.send(result);
+    });
+    // comment
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -46,7 +59,14 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-//     await client.close();
+    //     await client.close();
   }
 }
 run().catch(console.dir);
+app.get("/", (req, res) => {
+  res.send("Food server is running");
+});
+
+app.listen(port, () => {
+  console.log("food server runnning ---");
+});
